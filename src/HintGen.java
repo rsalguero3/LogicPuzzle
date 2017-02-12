c3a4458bb3f0f18474f63e2bd1a46709f7a5ffd2
grantozz <grant.osborn92@gmail.com>
2017-02-12 16:23:34 -0500

    fixed bug needs new hints

--- src/HintGen.java
+++ src/HintGen.java
@@ -1,5 +1,8 @@
+package logicpuzzle;        
+
 import java.util.ArrayList;
 import javafx.scene.effect.Bloom;
+import javafx.scene.layout.Background;
 
 /**
  *
@@ -8,27 +11,69 @@ import javafx.scene.effect.Bloom;
  * 
  */
 public class HintGen {
+
      private ArrayList<Hint> hints;
     
     public HintGen(){
         hints= new ArrayList<>();
-        hints.add(new Hint(2,0,0,"hint 1 Mark the highlighted cell as TRUE."));
-        hints.add(new Hint(1,1,0,"hint 2 Mark the highlighted cell as TRUE"));
-        hints.add(new Hint(0,2,2,"hint 3 Mark the highlighted cell as FALSE."));  
-     
+        hints.add(new Hint(0,1,0,"diamond ring and 325$ are explicitly true. Mark the highlighted cell as TRUE\n"));
+        hints.add(new Hint(0,1,2,"addison beach and 325$ are explicitly true. Mark the highlighted cell as TRUE\n"));
+        hints.add(new Hint(1,3,1,"we know the diamond ring was found at addison, so the earing must have been "
+                + "found at heffen lane. Mark the highlighted cell as true.\n"));  
+        
+        hints.add(new Hint(1,0,0,"the peice at heffen lane sold for less than the 325 diamond ring " 
+                + "beacuse we know the earing was found at heffenlane and the only thing less than 325 is 250 "
+                + "the earing must have sold for 250. Mark the highlighted cell as TRUE \n"));
+        
+        hints.add(new Hint(3,3,0,"the wristwatch sold for less than the object found at burr woods "
+                + "so the wrist watch can not be the most expensive. Mark the highlighted cell as FALSE \n"));
+        
+        hints.add(new Hint(2,3,0,"by process of eliminition we know the "
+                + "gold chain cost 475. Mark the highlighted cell as TRUE \n"));
+        
+        hints.add(new Hint(3,2,0,"by process of eliminition we know the "
+                + "wristwatch cost 400. Mark the highlighted cell as TRUE \n"));
+        
+        hints.add(new Hint(3,2,0,"by process of eliminition we know the "
+                + "wristwatch cost 400. Mark the highlighted cell as TRUE \n"));
+        
+         hints.add(new Hint(2,1,1,"the wristwatch sold for 75 less than the object found at "
+                 + "burr woods. the gold chain cost 75 more than the wirstwatch so it must have been found "
+                 + "at burr woods. Mark the highlighted cell as TRUE \n"));
+         
+         hints.add(new Hint(0,0,1,"we know the diamond sold for 325 and the "
+                 + "item that sold for 325 was found at addison beach. Mark the highlighted cell as TRUE \n"));
+         
+         hints.add(new Hint(3,2,1,"by process of eliminition we know the "
+                + "wristwatch was found at front beach . Mark the highlighted cell as TRUE \n"));
+         
+         hints.add(new Hint(3,0,2,"we know the earing cost 250 and was found at heffen lane "
+                 + "  Mark the highlighted cell as TRUE \n"));
+         
+         hints.add(new Hint(2,2,2,"we know the wristwatch cost 400 and was found at front beach"
+                 + "  Mark the highlighted cell as TRUE \n"));
+         
+         hints.add(new Hint(1,3,2,"by process of eliminition we know the "
+                + "object that cost 475  was found at burr woods . Mark the highlighted cell as TRUE \n"));          
     }   
     
     public String generate(GridModel g1,GridModel g2,GridModel g3){
         Hint myHint = getHint(g1, g2, g3);
             switch (myHint.getGridNum()) {
+                //error case
+                case-1:
+                    break;
                 case 0:
-                    g1.getButtonAt(myHint.getX(),myHint.getY()).setEffect(new Bloom());
+                    g1.getButtonAt(myHint.getX(),myHint.getY()).setStyle("-fx-min-height: 75px; -fx-min-width: 75px; "
+                                                                         + "-fx-background-color: #e5ff00;");
                     break;
                 case 1:
-                    g3.getButtonAt(myHint.getX(),myHint.getY()).setEffect(new Bloom());
+                    g3.getButtonAt(myHint.getX(),myHint.getY()).setStyle("-fx-min-height: 75px; -fx-min-width: 75px; "
+                                                                        + "-fx-background-color: #e5ff00;");
                     break;
                 default:
-                    g2.getButtonAt(myHint.getX(),myHint.getY()).setEffect(new Bloom());
+                    g2.getButtonAt(myHint.getX(),myHint.getY()).setStyle("-fx-min-height: 75px; -fx-min-width: 75px; "
+                                                                        + "-fx-background-color: #e5ff00;");
                     break;
             }
            return myHint.getMsg();   
@@ -44,7 +89,7 @@ public class HintGen {
                         return h;
                     }else{
                         if(!g1.clearErrors()){
-                            return new Hint(0,0,0,"you made a mistake"); 
+                            return new Hint(0,0,-1,"you made a mistake"); 
                         }
                     }    break;
                 case 1:
@@ -52,7 +97,7 @@ public class HintGen {
                         return h;
                     }else{
                         if(!g3.clearErrors()){
-                            return new Hint(0,0,0,"you made a mistake"); 
+                            return new Hint(0,0,-1,"you made a mistake"); 
                         }
                     }    break;
                 default:
@@ -60,13 +105,13 @@ public class HintGen {
                         return h;
                     }else{
                         if(!g2.clearErrors()){
-                            return new Hint(0,0,0,"you made a mistake"); 
+                            return new Hint(0,0,-1,"you made a mistake"); 
                         }
                     }   break;
             }
         }
         
-        return new Hint(0,0,0,"no more hints"); 
+        return new Hint(0,0,-1," you won! no more hints"); 
     }
       
 }
